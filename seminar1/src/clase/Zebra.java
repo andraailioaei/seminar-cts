129cffc3c634d0558cf05d2d5478136d1266bfe7
Andra <ailioaeiandra18@stud.ase.ro>
2021-02-25 13:30:25 +0200

    zebra

---
+++ seminar1/src/clase/Zebra.java
@@ -0,0 +1,16 @@
+package clase;
+
+public class Zebra extends Animal {
+	private int nrDungi;
+	
+	public Zebra(String name)
+	{
+		super(name);
+		this.nrDungi=200;
+	}
+
+	public Zebra(String name, int nrDungi) {
+		super(name);
+		this.nrDungi = nrDungi;
+	}
+}
--- seminar1/src/pachet/main.java
+++ seminar1/src/pachet/main.java
@@ -1,6 +1,7 @@
 package pachet;
 
 import clase.Giraffe;
+import clase.Zebra;
 import clase.Zoo;
 
 public class main {
@@ -9,9 +10,12 @@ public class main {
 		Zoo zoo = new Zoo();
 		Giraffe g1 = new Giraffe("Maia", 300);
 		Giraffe g2 = new Giraffe("Lola");
-		
+		Zebra z1 = new Zebra("Lili");
+		Zebra z2 = new Zebra("Mimi", 20);
 		zoo.addAnimal(g1);
 		zoo.addAnimal(g2);
+		zoo.addAnimal(z1);
+		zoo.addAnimal(z2);
 		zoo.feedAnimals();
 	}
 
